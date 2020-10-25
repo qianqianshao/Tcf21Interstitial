79476d7871f9262bd0bbb5df6fd443699dbde8e0
Qianyi Ma <qzm@umich.edu>
2020-10-25 19:13:22 -0400

    Update and rename INT4somaLeyDiff3TimePoints_Fig3A-C_TableS2.R to INT4LeyDiff3TimePoints_Fig3A-C_TableS2.R

--- R/INT4LeyDiff3TimePoints_Fig3A-C_TableS2.R
+++ R/INT4LeyDiff3TimePoints_Fig3A-C_TableS2.R
@@ -1,6 +1,6 @@
 # scRNA-seq analysis of in-vitro Leydig differentiation at 3 different time points by Qianyi in Nov 2019 
 # Related to Figure 3A-C and Table S2
-# merge the INT4 soma with 3 time points of in-vitro leydig differentiation data and do clustering
+# merged the INT4 soma (time point 0) with 3 time points of in-vitro leydig differentiation data and did clustering
 
 
 library(Seurat)
